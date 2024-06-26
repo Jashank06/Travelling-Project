769836a85437d814832b8200838bc4d63082824d
Nishtha Aggarwal <145250198+nishthaaggarwal15@users.noreply.github.com>
2024-06-27 01:34:29 +0530

    Updated

--- Meals.js
+++ Meals.js
@@ -1,14 +1,58 @@
 // scripts.js
 
-document.addEventListener('DOMContentLoaded', () => {
-    const images = document.querySelectorAll('.hero-images img');
-    let currentImageIndex = 0;
+// scripts.js
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const slides = document.querySelector('.slides');
+let index = 0;
 
-    function changeImage() {
-        images[currentImageIndex].classList.remove('active');
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        images[currentImageIndex].classList.add('active');
+next.addEventListener('click', () => {
+    index = (index < slides.children.length - 1) ? index + 1 : 0;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+});
+
+prev.addEventListener('click', () => {
+    index = (index > 0) ? index - 1 : slides.children.length - 1;
+    slides.style.transform = `translateX(-${index * 100}%)`;
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const heroImages = document.querySelectorAll('.hero-images img');
+    let currentHeroImageIndex = 0;
+
+    function changeHeroImage() {
+        heroImages[currentHeroImageIndex].classList.remove('active');
+        currentHeroImageIndex = (currentHeroImageIndex + 1) % heroImages.length;
+        heroImages[currentHeroImageIndex].classList.add('active');
     }
 
-    setInterval(changeImage, 5000); // Change image every 5 seconds
+    setInterval(changeHeroImage, 5000); // Change hero image every 5 seconds
+
+    const reservationImages = document.querySelectorAll('.image-container img');
+    let currentReservationImageIndex = 0;
+
+    function changeReservationImage() {
+        reservationImages[currentReservationImageIndex].classList.remove('active');
+        currentReservationImageIndex = (currentReservationImageIndex + 1) % reservationImages.length;
+        reservationImages[currentReservationImageIndex].classList.add('active');
+    }
+
+    setInterval(changeReservationImage, 5000); // Change reservation image every 5 seconds
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const diningOptions = document.querySelectorAll('.dining-options .option');
+    let currentIndex = 0;
+
+    function showDiningOption(indexToShow) {
+        diningOptions.forEach((option, index) => {
+            if (index === indexToShow) {
+                option.style.display = 'block';
+            } else {
+                option.style.display = 'none';
+            }
+        });
+    }
+
+
 });
